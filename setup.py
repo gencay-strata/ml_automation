3b2da107c55cbf0e936d6fc76279f2bc1fa34587
gencay-strata <130021280+gencay-strata@users.noreply.github.com>
2024-08-22 10:46:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ml_automation',
-    version='0.1.0',
+    name='ml-algo-automation',
+    version='0.1.5',  
     packages=find_packages(),
     install_requires=[
         'scikit-learn',
         'numpy',
     ],
-    author='Gencay',
-    author_email='mehmet@stratascratch.com',
+    author='Nathan Rosidi',
+    author_email='nate@stratascratch.com',
     description='A library for comparing regression and classification models using scikit-learn.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -21,3 +21,4 @@ setup(
     ],
     python_requires='>=3.6',
 )
+

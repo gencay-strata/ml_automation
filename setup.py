c840276cda07181ba5161e87109dec366e2dbc01
gencay-strata <130021280+gencay-strata@users.noreply.github.com>
2024-08-22 08:41:33 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_automation',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A library for comparing regression and classification models using scikit-learn.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gencay-strata/ml_automation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
